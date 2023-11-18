293bb710fe8847b01b2cefa35f48f01b37737bb3
scherpablo <scherpablo@gmail.com>
2023-11-18 10:08:07 -0300

    upload kiosco cole app

---
+++ prisma/data/categorias.ts
@@ -0,0 +1,28 @@
+const categorias = [
+  {
+    icono: "menus",
+    nombre: "Menús",
+  },
+  {
+    icono: "sandwiches",
+    nombre: "Sandwiches",
+  },
+  {
+    icono: "bebidas",
+    nombre: "Bebidas",
+  },
+  {
+    icono: "golosinas",
+    nombre: "Golosinas",
+  },
+  {
+    icono: "helados",
+    nombre: "Helados",
+  },
+  {
+    icono: "libreria",
+    nombre: "Librerría",
+  },
+];
+
+export { categorias };
