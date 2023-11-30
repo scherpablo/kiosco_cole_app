982583b5ec46f80f02ba5d1601be3c3fc68896a3
scherpablo <scherpablo@gmail.com>
2023-11-30 06:29:36 -0300

    edit admin

--- pages/pedidos.jsx
+++ pages/pedidos.jsx
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import axios from "axios";
 import AdminLayout from "../layout/admin/AdminLayout";
 import Pedido from "../components/Pedido/Pedido";
-import styles from "./donluis.module.css";
+import styles from "./pedidos.module.css";
 
 export default function Admin() {
   const fetcher = () => axios("/api/pedidos").then((datos) => datos.data);
